5b1e49773b9fea105857554eab4b5ad3c166a873
Andrew Lima <andrew@arctek.co.za>
2016-08-29 20:49:04 +0200

    Added administrator widget
    Added administrator widget

--- when-last-login.php
+++ when-last-login.php
@@ -3,12 +3,15 @@
 Plugin Name: When Last Login
 Plugin URI: https://wordpress.org/plugins/when-last-login/
 Description: Adds functionality to your WordPress install to show when a user last logged in.
-Version: 0.3
+Version: 0.4
 Author: Arctek Technologies (Pty) Ltd
 Author URI: http://www.whenlastlogin.com
 Text Domain: when-last-login
 Domain Path: /languages
 
+  * 0.4 29-08-206
+  * Enhancement: Implemented widgetto display top logged in users
+  *
   * 0.3 22-07-2016
   * Enhancement: Implemented multi language support and a couple of language files.
   * Language Support: French, Spanish, German and Italian
@@ -38,6 +41,9 @@ class When_Last_Login {
       //Create the custom meta upon login
       add_action( 'wp_login', array( 'When_Last_Login', 'last_login'), 10, 2);
 
+      //Show admin metabox
+      add_action( 'wp_dashboard_setup', array( 'When_Last_Login', 'admin_dashboard_widget' ));
+
       //Setting up columns.
       add_filter( 'manage_users_columns', array( 'When_Last_Login', 'column_header'), 10, 1);
       add_action( 'manage_users_custom_column', array( 'When_Last_Login', 'column_data'), 15, 3);
@@ -45,6 +51,7 @@ class When_Last_Login {
       add_action( 'pre_get_users', array('When_Last_Login', 'sort_by_login_date'));
 
       //Integration for Paid Memberships Pro
+      //TODO: Improve integration with Member List and Paid Memberships Pro
       add_action('pmpro_memberslist_extra_cols_header', array( 'When_Last_Login', 'pmpro_memberlist_add_header' ));
       add_action('pmpro_memberslist_extra_cols_body', array('When_Last_Login', 'pmpro_memberlist_add_column_data' ));
 
@@ -74,11 +81,62 @@ class When_Last_Login {
     }
 
      public static function last_login( $user_login, $users ){
+
        //get/update user meta 'when_last_login' on login and add time() to it.
        update_user_meta( $users->ID, 'when_last_login', time() );
+
+       //get and update user meta 'when_last_login_count' on login for # of login counts. Thanks to Jarryd Long (@jarrydlong) from Code Cabin (@code_cabin) for the assistance
+       $wll_count = get_user_meta( $users->ID, 'when_last_login_count', true );
+
+       if($wll_count === false){
+         update_user_meta($users->ID, 'when_last_login_count', 1);
+       }else{
+         $wll_new_value = intval($wll_count);
+         $wll_new_value = $wll_new_value + 1;
+
+         update_user_meta($users->ID, 'when_last_login_count', $wll_new_value);
+       }
      }
 
-     /*
+     /**
+     * Setup admin backend to display custom meta box for login count for admins
+     */
+     public static function admin_dashboard_widget(){
+       //only show for administrators
+       if(current_user_can('manage_options')){
+        wp_add_dashboard_widget('when_last_login_top_users', __( 'Top 3 Users', 'when-last-login' ), array( 'When_Last_Login', 'admin_dashboard_widget_display' ));
+       }
+     }
+
+     public static function admin_dashboard_widget_display(){
+
+      $user_query = new WP_User_Query( array( 'meta_key' => 'when_last_login_count', 'meta_value' => 0, 'meta_compare' => '!=', 'order' => 'ASC', 'oderby' => 'meta_value', 'number' => 3) );
+
+      $topusers = $user_query->get_results();
+
+       if($topusers){
+?>
+         <table width="100%" text-align="center">
+           <tr>
+             <td><b><span style="font-size:18px;"><?php _e( 'Users', 'when-last-login' ); ?></span></b></td>
+             <td><b><span style="font-size:18px;"><?php _e( 'Login Count', 'when-last-login' ); ?></span><b></td>
+           </tr>
+<?php
+         foreach($topusers as $wllusers){
+            echo '<tr><td>' . $wllusers->display_name . '</td>';
+            echo '<td>' . get_user_meta( $wllusers->ID, 'when_last_login_count', true ) . '</td></tr>';
+        }
+?>
+      </table>
+      <br/>
+      <a href="<?php echo site_url('/wp-admin/users.php?orderby=when_last_login&order=desc'); ?>"><?php _e( 'View All Users', 'when-last-login' ); ?></a>
+<?php
+      }else{
+       echo 'No data yet';
+     }
+}
+
+     /**
      * Setup Column and data for users page with sortable
      */
      public static function column_header( $column ){
@@ -89,11 +147,8 @@ class When_Last_Login {
      public static function column_data( $value, $column_name, $id ){
       if ($column_name == 'when_last_login'){
         $when_last_login_meta = get_the_author_meta('when_last_login', $id);
-
         if(!empty($when_last_login_meta)){
-
           return human_time_diff($when_last_login_meta);
-
         }else{
 
           if(get_the_author_meta('when_last_login', $id) === 0){
@@ -115,15 +170,16 @@ class When_Last_Login {
     public static function sort_by_login_date( $query ) {
       if ( 'when_last_login' == $query->get( 'orderby' ) ) {
         $query->set( 'orderby', 'meta_value_num' );
-        $query->set( 'meta_key', 'when_last_login');
+        $query->set( 'meta_key', 'when_last_login' );
       }
     }
 
      /*
      * Support for Paid Memberships Pro
+     * TODO: use existing PMPro usermeta if installed
      */
      public static function pmpro_memberlist_add_header( $users ){
-       if( !defined('PMPRO_VERSION') ){
+       if( !defined( 'PMPRO_VERSION' ) ){
          return;
        }
 ?>
@@ -132,7 +188,7 @@ class When_Last_Login {
      }
 
      public static function pmpro_memberlist_add_column_data( $users ){
-       if( !defined('PMPRO_VERSION') ){
+       if( !defined( 'PMPRO_VERSION' ) ){
          return;
        }
 ?>
@@ -141,7 +197,7 @@ class When_Last_Login {
       if(!empty( $users->when_last_login )){
         echo human_time_diff( $users->when_last_login );
       }else{
-        return _e('Never', 'when-last-login');
+        return _e( 'Never', 'when-last-login' );
       }
 ?>
       </td>
