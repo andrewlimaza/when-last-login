362ace49de166984f47db2c5768938ec68d69550
Andrew Lima <andrew@arctek.co.za>
2018-04-24 16:07:12 +0200

    Clean up database if delete
    Cleans up the database on plugin delete. Also deletes custom table if it's found. This was bug in 0.9

--- uninstall.php
+++ uninstall.php
@@ -5,6 +5,8 @@ if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit();
 }
 
+global $wpdb;
+
 $users_id = get_users( array(
   'fields' => 'ID'
 ) );
@@ -13,3 +15,16 @@ foreach( $users_id as $user_id ){
   delete_user_meta( $user_id, 'when_last_login' );
   delete_user_meta( $user_id, 'when_last_login_count' );
 }
+
+//Delete CPT's from databse if you uninstall When Last Login.
+$sql = "DELETE FROM $wpdb->posts WHERE post_type='wll_records'";
+$wpdb->query( $sql );
+
+//Delete custom table if it exists
+$delete_table = $wpdb->prefix . 'users_copy';
+$drop_if_exists = "DROP TABLE IF EXISTS $delete_table";
+$wpdb->query( $drop_if_exists );
+
+//clear up all options from 'options' table.
+$sqlQuery = "DELETE FROM $wpdb->options WHERE option_name LIKE 'wll%'";
+$wpdb->query($sqlQuery);
